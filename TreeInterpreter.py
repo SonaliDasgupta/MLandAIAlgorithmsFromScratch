e9f144d7f56fd99d30c669dcba11308ddd11e4d2
Sonali Dasgupta <sonalidasgupta95.2011@gmail.com>
2018-12-02 20:23:09 +0530

    Update TreeInterpreter.py

--- TreeInterpreter.py
+++ TreeInterpreter.py
@@ -5,3 +5,11 @@ class TreeInterpreter:
             print('prediction: '+str(prediction)+"bias: "+str(bias)+" contributions: "+str(contribs))
            
             return prediction, bias, contribs
+
+        
+#Using the Tree Interpreter for our Random Forest
+ti = TreeInterpreter()
+pr, bias, contribs  = ti.predict(rf_mine.trees[0], X_valid.iloc[0, :])
+
+from waterfall import waterfall_chart
+waterfall_chart.plot(list(contribs.keys()), list(contribs.values()))
